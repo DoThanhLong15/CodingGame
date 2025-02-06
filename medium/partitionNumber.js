fee62f05424b04d68aa67c7d4032cae6ae2da921
DoThanhLong15 <108256260+DoThanhLong15@users.noreply.github.com>
2025-02-06 14:22:05 +0700

    solve partitions number puzzle

---
+++ medium/partitionNumber.js
@@ -0,0 +1,18 @@
+
+const T = parseInt(readline());
+
+for (let i = 0; i < T; i++) {
+    const n = parseInt(readline());
+
+    const numberCase = new Array(n + 1).fill(0)
+    numberCase[0] = 1;
+
+    for (let value = 1; value <= n; value++) {
+        for(let remain = value; remain <= n; remain++) {
+
+            numberCase[remain] += numberCase[remain - value];
+        }
+    }
+
+    console.log(numberCase[n]);
+}
